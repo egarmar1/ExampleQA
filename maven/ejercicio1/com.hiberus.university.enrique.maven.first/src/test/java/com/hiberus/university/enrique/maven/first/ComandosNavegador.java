3c55ecb63508caa7e4c6079604a2f3746beaea04
unknown <egarmar1@teleco.upv.es>
2023-04-20 18:58:26 +0200

    Ejercicio Comandos Navegador terminado

--- maven/ejercicio1/com.hiberus.university.enrique.maven.first/src/test/java/com/hiberus/university/enrique/maven/first/ComandosNavegador.java
+++ maven/ejercicio1/com.hiberus.university.enrique.maven.first/src/test/java/com/hiberus/university/enrique/maven/first/ComandosNavegador.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class ComandosNavegador {
-    private static String originalUrl = "https://www.saucedemo.com";
+    private static String originalUrl = "https://www.saucedemo.com/";
 
     public static void main(String[] args) throws InterruptedException{
         WebDriver driver;
@@ -16,24 +16,25 @@ public class ComandosNavegador {
         driver = new FirefoxDriver(firefoxOptions);
         driver.manage().window().maximize();
 
-        driver.get("https://www.saucedemo.com");
+        driver.get("https://www.saucedemo.com/");
 
         String titulo= driver.getTitle();
         int titleLength= titulo.length();
-        System.out.println("Title= " + titulo + "Length=" + titleLength);
+        System.out.println("Title= " + titulo + ". Length=" + titleLength);
 
         String url = driver.getCurrentUrl();
-
 
 
         if (url.equals(originalUrl)){
             System.out.println(true);
         }
 
-        String source = driver.getPageSource();
-        int sourceLength= titulo.length();
 
-        System.out.println("La pantalla tiene una longitud de" + sourceLength);
+
+        String source = driver.getPageSource();
+        int sourceLength= source.length();
+
+        System.out.println("El codigo fuente tiene una longitud de " + sourceLength);
         driver.quit();
     }
 }
